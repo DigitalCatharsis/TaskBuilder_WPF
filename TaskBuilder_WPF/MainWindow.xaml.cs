82f197a171b9199bfd26c370384924f2af454fdc
DigitalCatharsis <shinegumy@gmail.com>
2023-04-26 12:56:57 +0300

    Changed gitignore from regular C# to C# WPF
    Added some UI (practicing)

---
+++ TaskBuilder_WPF/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskBuilder_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Selection(object sender, RoutedEventArgs e)
+        {
+            var w_SelectionWindow = new SelectionWindow();
+            w_SelectionWindow.Show();
+            Hide();
+        }
+        private void Button_Click_Start(object sender, RoutedEventArgs e)
+        {
+            var w_StartWindow = new StartWindow();
+            w_StartWindow.Show();
+            Hide();
+        }
+        private void Button_Click_Preview(object sender, RoutedEventArgs e)
+        {
+            var w_PreviewWindow = new PreviewWindow();
+            w_PreviewWindow.Show();
+            Hide();
+        }
+        private void Button_Click_Test(object sender, RoutedEventArgs e)
+        {
+            var w_TestWindow = new TestWindow();
+            w_TestWindow.Show();
+            Hide();
+        }
+
+        private void Button_Click_EscButton(object sender, RoutedEventArgs e)
+        {
+            this.Close(); // закрытие окна
+        }
+
+        private void Button_Click_Selection2(object sender, RoutedEventArgs e)
+        {
+            var w_SelectionWindow2 = new SelectionWindow2();
+            w_SelectionWindow2.Show();
+            Hide();
+        }
+    }
+
+    
+
+}
