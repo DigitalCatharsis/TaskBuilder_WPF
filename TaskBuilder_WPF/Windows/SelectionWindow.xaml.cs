7e19259f5f8815af1f8913cec70df6cbe0f52536
DigitalCatharsis <shinegumy@gmail.com>
2023-05-05 14:53:00 +0300

    1.0.1 Fixed Scroll in SelectionWindow

--- TaskBuilder_WPF/Windows/SelectionWindow.xaml.cs
+++ TaskBuilder_WPF/Windows/SelectionWindow.xaml.cs
@@ -46,6 +46,13 @@ namespace TaskBuilder_WPF
             Hide();
         }
 
+        private void ListViewScrollViewer_PreviewMouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        {
+            ScrollViewer scv = (ScrollViewer)sender;
+            scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta);
+            e.Handled = true;
+        }
+
 
     }
 }
